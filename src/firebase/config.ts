942a6e1545dc89e56268c4cbf3bfb938fd20b6ae
Warrior Tech System <warriortechsystem@gmail.com>
2026-04-03 03:12:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3002446330-d43c8",
+  "appId": "1:1024789679712:web:b54cd61dcd21fce715fd05",
+  "apiKey": "AIzaSyAEyCM4Ay93zrxMjSWU5h5Dcr7HkPF3rUE",
+  "authDomain": "studio-3002446330-d43c8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1024789679712"
+};
